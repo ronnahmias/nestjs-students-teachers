c39f90a4f1b09a6e2be5bba6295f6951102f0750
ron212job <ron212.job@gmail.com>
2022-03-07 19:22:00 +0200

    split app to modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
-import { StudentController } from "../student/student.controller"
-import { TeacherController } from "../teacher/teacher.controller"
-import { StudentTeacherController } from "../teacher/student.constroller"
-import { StudentService } from 'src/student/student.service';
-import { TeacherService } from 'src/teacher/teacher.service';
+import { StudentModule } from 'src/student/student.module';
+import { TeacherModule } from 'src/teacher/teacher.module';
 
 // main app starting
 
 @Module({
-  imports: [],
-  controllers: [StudentController, TeacherController,StudentTeacherController],
-  providers: [StudentService, TeacherService]
+  imports: [StudentModule, TeacherModule],
 })
 export class AppModule {}
